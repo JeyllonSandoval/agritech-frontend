043f5695af26ef695e4c8f4fb90654fa5befb198
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-03-04 21:48:32 -0400

    feat: create Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
-export default function Home() {
+import Home from "./modules/Home/Home";
+export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
