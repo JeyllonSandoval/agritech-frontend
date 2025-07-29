ff1f946c17682adaf2704158f4fb71830a3b9128
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-07-28 22:35:23 -0400

    feat: enhance About page layout and content; implement smooth scrolling navigation, add background effects, and improve section rendering for better user experience

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import AboutContent from "@/components/common/UI/about/AboutContent";
 
 export default function AboutPage() {
     return (
-        <div className="flex justify-center items-center w-full min-h-screen px-4 md:px-6 lg:py-0">
+        <div className="w-full min-h-screen">
             <AboutContent />
         </div>
     );
